f76b7b9e474ae44a509cf603b5c87173e230b1b1
unknown <ahedqaood19@gmail.com>
2022-05-16 02:08:44 +0300

    update

--- test.py
+++ test.py
@@ -47,6 +47,50 @@ while True:
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
+
+    orig = frame.copy()
+    (origH, origW) = frame.shape[:2]
+    # set the new width and height and then determine the ratio in change
+    # for both the width and height
+    (newW, newH) = (args["width"], args["height"])
+    rW = origW / float(newW)
+    rH = origH / float(newH)
+    # resize the image and grab the new image dimensions
+    frame = cv2.resize(frame, (newW, newH))
+    (H, W) = frame.shape[:2]
+    # construct a blob from the image and then perform a forward pass of
+    # the model to obtain the two output layer sets
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
+        (123.68, 116.78, 103.94), swapRB=True, crop=False)
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+    # decode the predictions, then  apply non-maxima suppression to
+    # suppress weak, overlapping bounding boxes
+    (rects, confidences) = decode_predictions(scores, geometry)
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
+    if(not isinstance(boxes, list)):
+        box = np.array(
+        [np.amin(boxes, axis=0)[0], np.amin(boxes, axis=0)[1], 
+        np.amax(boxes, axis=0)[2], np.amax(boxes, axis=0)[3]]
+        )
+        # loop over the bounding boxes
+        startX, startY, endX, endY = box
+        # scale the bounding box coordinates based on the respective
+        # ratios
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+        # in order to obtain a better OCR of the text we can potentially
+        # apply a bit of padding surrounding the bounding box -- here we
+        # are computing the deltas in both the x and y directions
+        dX = int((endX - startX) * args["padding"])
+        dY = int((endY - startY) * args["padding"])
+        # apply padding to each side of the bounding box, respectively
+        startX = max(0, startX - dX)
+        startY = max(0, startY - dY)
+        endX = min(origW, endX + (dX * 2))
+        endY = min(origH, endY + (dY * 2))
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
