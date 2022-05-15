ca6e0ac40665f35842bc3a1bbe87e963ec77f52b
unknown <ahedqaood19@gmail.com>
2022-05-16 02:06:38 +0300

    update

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
 import numpy as np
 import cv2
+import argparse
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str,
+    help="path to input image")
+ap.add_argument("-east", "--east", type=str, default='frozen_east_text_detection.pb',
+    help="path to input EAST text detector")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+    help="minimum probability required to inspect a region")
+ap.add_argument("-w", "--width", type=int, default=320,
+    help="nearest multiple of 32 for resized width")
+ap.add_argument("-e", "--height", type=int, default=320,
+    help="nearest multiple of 32 for resized height")
+ap.add_argument("-p", "--padding", type=float, default=0.0,
+    help="amount of padding to add to each border of ROI")
+args = vars(ap.parse_args())
+
+# define the two output layer names for the EAST detector model that
+# we are interested in -- the first is the output probabilities and the
+# second can be used to derive the bounding box coordinates of text
+layerNames = [
+    "feature_fusion/Conv_7/Sigmoid",
+    "feature_fusion/concat_3"]
+
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet(args["east"])
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
