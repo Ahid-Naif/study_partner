8c9465ffcd07fae19330d3116ea9dcbf779d6bfc
unknown <ahedqaood19@gmail.com>
2022-05-16 01:59:20 +0300

    update

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -16,9 +16,9 @@ while True:
     # Our operations on the frame come here
 
     # Display the resulting frame
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
