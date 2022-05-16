e9ec6dc6b1822677f0a1fceaf1844e34d929be01
unknown <ahedqaood19@gmail.com>
2022-05-16 03:58:39 +0300

    update

--- run_raspberry.py
+++ run_raspberry.py
@@ -189,6 +189,7 @@ def ocrProgram():
     global pressed
     global long_pressed
     global vs
+    global n_boxes
     print("[INFO] starting video stream...")
     vs.start()
     start_time = time.time()    
@@ -201,6 +202,19 @@ def ocrProgram():
             ocr_result = []
             d = pytesseract.image_to_data(frame, output_type=Output.DICT)
             n_boxes = len(d['text'])
+            for i in range(n_boxes):
+                if int(d['conf'][i]) > 60:
+                    ocr_result.append(d['text'][i])
+                    (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                    # don't show empty text
+                    if text and text.strip() != "":
+                        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            ocr_result = " ".join(ocr_result)
+            tb = TextBlob(ocr_result)
+            text = tb.correct()
+            start_time = time.time()
+        else:
             for i in range(n_boxes):
                 if int(d['conf'][i]) > 60:
                     ocr_result.append(d['text'][i])
