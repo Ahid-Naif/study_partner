0a38d8337d8b0298e77479f0043cb72cca2bc70c
unknown <ahedqaood19@gmail.com>
2022-05-16 03:55:50 +0300

    update

--- run_raspberry.py
+++ run_raspberry.py
@@ -191,26 +191,29 @@ def ocrProgram():
     global vs
     print("[INFO] starting video stream...")
     vs.start()
-    time.sleep(2)
+    start_time = time.time()    
     while True:
         # Capture frame-by-frame
         frame = vs.read()
         frame = imutils.resize(frame, width=400)
-        ocr_result = []
+        
+        if time.time() - start_time > 5:
+            ocr_result = []
+            d = pytesseract.image_to_data(frame, output_type=Output.DICT)
+            n_boxes = len(d['text'])
+            for i in range(n_boxes):
+                if int(d['conf'][i]) > 60:
+                    ocr_result.append(d['text'][i])
+                    (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                    # don't show empty text
+                    if text and text.strip() != "":
+                        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        d = pytesseract.image_to_data(frame, output_type=Output.DICT)
-        n_boxes = len(d['text'])
-        for i in range(n_boxes):
-            if int(d['conf'][i]) > 60:
-                ocr_result.append(d['text'][i])
-                (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-                # don't show empty text
-                if text and text.strip() != "":
-                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-        ocr_result = " ".join(ocr_result)
-        tb = TextBlob(ocr_result)
-        text = tb.correct()
+            ocr_result = " ".join(ocr_result)
+            tb = TextBlob(ocr_result)
+            text = tb.correct()
+            start_time = time.time() 
+
         # Display the resulting frame
         cv2.imshow('frame', frame)
         cv2.waitKey(1)
